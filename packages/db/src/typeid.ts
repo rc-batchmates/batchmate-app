5687507e1884bd5debf4ca730f0fd69d6415b426
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2026-03-29 17:16:08 -0400

    lint db

--- packages/db/src/typeid.ts
+++ packages/db/src/typeid.ts
@@ -1,23 +1,19 @@
-import { customType } from "drizzle-orm/sqlite-core";
-import {
-	fromString,
-	type TypeId,
-	typeidUnboxed,
-} from "typeid-js";
+import { customType } from "drizzle-orm/sqlite-core"
+import { fromString, type TypeId, typeidUnboxed } from "typeid-js"
 
-export { fromString as parseTypeId, type TypeId } from "typeid-js";
+export { fromString as parseTypeId, type TypeId } from "typeid-js"
 
 export const typeIdColumn = <T extends string>(prefix: T) =>
 	customType<{ data: TypeId<T> }>({
 		dataType: () => "text",
 		fromDriver: (val: unknown) => fromString(val as string, prefix),
 		toDriver: (val: string) => String(val),
-	});
+	})
 
 export const typeIdPrimaryKey = <T extends string>(prefix: T) => {
-	const col = typeIdColumn(prefix);
+	const col = typeIdColumn(prefix)
 	return (name: string) =>
 		col(name)
 			.primaryKey()
-			.$defaultFn(() => typeidUnboxed(prefix));
-};
+			.$defaultFn(() => typeidUnboxed(prefix))
+}
