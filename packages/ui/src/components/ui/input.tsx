8824fe4d1a719a66e2a5445c5955ed4a37697dab
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2026-03-29 17:16:21 -0400

    lint ui

--- packages/ui/src/components/ui/input.tsx
+++ packages/ui/src/components/ui/input.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import type * as React from "react"
 import { TextInput } from "react-native"
 
 import { cn } from "../../lib/cn"
--- packages/ui/src/components/ui/label.tsx
+++ packages/ui/src/components/ui/label.tsx
@@ -1,5 +1,5 @@
 import * as LabelPrimitive from "@rn-primitives/label"
-import * as React from "react"
+import type * as React from "react"
 
 import { cn } from "../../lib/cn"
 
