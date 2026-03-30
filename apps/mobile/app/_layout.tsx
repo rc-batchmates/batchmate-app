142dcd58894eb21ca6590b451e6a6ba48bebede4
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2026-03-29 20:56:10 -0400

    add bg

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useFonts } from "expo-font"
 import { Slot } from "expo-router"
 import { StatusBar } from "expo-status-bar"
+import { View } from "react-native"
 import { SafeAreaProvider } from "react-native-css/components/react-native-safe-area-context"
 
 const queryClient = new QueryClient()
@@ -15,11 +16,13 @@ export default function RootLayout() {
 	})
 
 	return (
-		<SafeAreaProvider>
-			<QueryClientProvider client={queryClient}>
-				<StatusBar style="light" />
-				<Slot />
-			</QueryClientProvider>
-		</SafeAreaProvider>
+		<View className="flex-1 bg-background">
+			<SafeAreaProvider>
+				<QueryClientProvider client={queryClient}>
+					<StatusBar style="light" />
+					<Slot />
+				</QueryClientProvider>
+			</SafeAreaProvider>
+		</View>
 	)
 }
