ed9198f8a8327c0dea3f6ee737473fc2ad971746
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2026-03-29 23:26:38 -0400

    setup drizzle studio

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,7 +1,13 @@
+import { globSync } from "node:fs"
 import { defineConfig } from "drizzle-kit"
+
+const d1DbPath = globSync(
+	"../../.wrangler/state/v3/d1/miniflare-D1DatabaseObject/*.sqlite",
+)[0]
 
 export default defineConfig({
 	schema: "./src/schema.ts",
 	out: "./drizzle",
 	dialect: "sqlite",
+	...(d1DbPath && { dbCredentials: { url: d1DbPath } }),
 })
